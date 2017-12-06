9881af6a6321ac328d2e4420c16d07b2a2773bb8
Hans Tercek <hanstercek@wr-130-64-218-39.medford.tufts.edu>
2017-12-06 00:34:48 -0500

    Add transitions and whatnot

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
 $( document ).ready(function() {
-
     // Baseline pages layout
     var numPages = 15;
     var pageWidth = $('#tmp-page').width();
+    var pageHeight = $('#tmp-page').height();
     var pagesWidth = (numPages - 2) * pageWidth;
     var winWidth = $(window).width()
 
     $('#page-wrapper').css('right', -pagesWidth);
-    $('#page-cover').css('width', winWidth - (pageWidth * 2));
+    $('#page-wrapper').css('height', pageHeight)
+    // $('#page-cover').css('width', winWidth - (pageWidth * 2));
 
     // Populate pages
     loadPages(numPages);
@@ -19,12 +20,54 @@ $( document ).ready(function() {
         var disp = (navToggle) ? 'visible' : 'hidden';
         var navSize = (navToggle) ? '.5em' : '1.5em';
         var navLeft = (navToggle) ? '0px' : '60px';
+        var navTop = (navToggle) ? '-5px' : '50%';
+        var navLeft = (navToggle) ? '0px' : '50%';
+        var navTranslate = (navToggle) ? 'translate(0%, 0%)' : 'translate(-50%, -50%)';
         var maskBack = (navToggle) ? 'none': 'no-repeat center/60% url("img/pixel_hand.png")';
+        var buttonWidth = (navToggle) ? '50px' : '100vw';
+        var buttonHeight = (navToggle) ? '50px' : '100vh'
         $('#nav-wrapper').css({'opacity': op});
         $('#nav-mask').css({'background': maskBack, 'opacity': op});
-        $('#nav').css({'font-size': navSize, 'left': navLeft});
+        $('#nav-button').css({'width': buttonWidth, 'height': buttonHeight});
+        $('#nav').css({'font-size': navSize, 'left': navLeft, 'top': navTop, 'transform': navTranslate});
         navToggle = !navToggle;
     });
+
+    // REPLACE WITH MODULAR FUNCTION
+    var pages = true;
+    var about = false;
+    var contact = false;
+    $('#about').click(function() {
+        if (pages == true) {
+            $('#page-wrapper').css('opacity', 0);
+            pages = false };
+        if (contact == true) {
+            $('#contact-wrapper').css('opacity', 0);
+            contact = false};
+        $('#about-wrapper').css('opacity', 1);
+        about = true;
+    });
+    $('#contact').click(function() {
+        if (pages == true) {
+            $('#page-wrapper').css('opacity', 0);
+            pages = false };
+        if (about == true) {
+            $('#about-wrapper').css('opacity', 0);
+            contact = false};
+        $('#contact-wrapper').css('opacity', 1);
+        contact = true;
+    });
+    $('#title').click(function() {
+        if (about == true) {
+            $('#about-wrapper').css('opacity', 0);
+            pages = false };
+        if (contact == true) {
+            $('#contact-wrapper').css('opacity', 0);
+            contact = false};
+        $('#page-wrapper').css('opacity', 1);
+        pages = true;
+    });
+
 
     // (function() {
     //     function scrollHorizontally(e) {
@@ -48,7 +91,7 @@ $( document ).ready(function() {
 
 function loadPages(numPages) {
     for (var i = 1; i < numPages; i++) {
-        $('#page-wrapper').append('<div class="page" id="page' + i + '"><img src="img/zines/zine1/page' + i + '.jpg" alt="page' + i + '" /></div>');
+        $('#page-wrapper').append('<img class="page" id="page' + i + '" src="img/zines/zine1/page' + i + '.jpg" alt="page' + i + '" />');
     }
     $('.photo').show();
     console.log($('#page-wrapper').css('width'));
